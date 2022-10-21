24dfa36fc19a1ec1ba358bf9fca36f20905e95a7
IvanMain <ivan.main.developer@gmail.com>
2022-10-21 09:42:47 +0300

    Отрисуй меня полностью (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 import { COUNT_CARDS_PHOTOS } from './constants.js';
 import { getCollectionCardsPhotos } from './data.js';
+import { renderCardsPhotos } from './renderCardsPhotos.js';
 
-getCollectionCardsPhotos(COUNT_CARDS_PHOTOS);
+const dataCardsPhotos = getCollectionCardsPhotos(COUNT_CARDS_PHOTOS);
+
+const containerCardsPhotos = document.querySelector('.pictures');
+const cardsPhotosList = renderCardsPhotos(dataCardsPhotos);
+
+containerCardsPhotos.append(cardsPhotosList);
