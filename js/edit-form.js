b390b59caa7d54ef88b37630f272b080a57b5802
IvanMain <ivan.main.developer@gmail.com>
2022-11-17 04:12:45 +0300

    fix

---
+++ js/edit-form.js
@@ -0,0 +1,44 @@
+const body = document.body;
+const modalEditForm = document.querySelector('#upload-select-image');
+const uloadFileInput = document.querySelector('#upload-file');
+const modalEditPhoto = document.querySelector('.img-upload__overlay');
+const modalCloseBtn = document.querySelector('.img-upload__cancel');
+
+const closeModalEditPhoto = () => {
+  modalEditPhoto.classList.add('hidden');
+  body.classList.remove('modal-open');
+  uloadFileInput.value = '';
+
+  modalCloseBtn.removeEventListener('click', closeModalEditPhoto);
+};
+
+const showModalEditPhoto = () => {
+  modalEditPhoto.classList.remove('hidden');
+  body.classList.add('modal-open');
+
+  modalCloseBtn.addEventListener('click', closeModalEditPhoto);
+};
+
+uloadFileInput.addEventListener('change', showModalEditPhoto);
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    closeModalEditPhoto();
+  }
+});
+
+const pristine = new Pristine(modalEditForm, {
+  classTo: 'img-upload__text',
+  errorTextParent: 'img-upload__text',
+  errorTextClass: 'text__error',
+});
+
+modalEditForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  const isValidLengthDescField = pristine.validate();
+
+  if (isValidLengthDescField) {
+    evt.currentTarget.submit();
+  }
+});
